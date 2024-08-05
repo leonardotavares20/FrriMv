1fe417c4d5a1e1620544b9469dde8b2812e02f81
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-05 03:01:23 -0300

    feat: add main +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="app">
+  <main>
+    <slot></slot>
+  </main>
+  <footer></footer>
+</div>
+
+<style>
+  .app {
+    @apply h-full;
+  }
+</style>
