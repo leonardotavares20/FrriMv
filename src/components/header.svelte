092b5802b29652af922f3f33c4070e61b7ed2252
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-20 21:17:43 -0300

    refactor: home - adjust home

---
+++ src/components/header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<header class="fixed w-[100vw] h-[10px] z-20 top-6 header">
+  <div
+    class="flex justify-between px-5 text-[12px] text-hells_red font-futura_lt uppercase cursor-pointer"
+  >
+    <div class="w-[20vw] max-w-[150px] flex justify-between">
+      <a class="hover:underline" href="/">Ferrari</a>
+      <a class="hover:underline" href="/">Get Tickets</a>
+    </div>
+    <div class="flex w-[20vw] pr-3 max-w-[180px] justify-between">
+      <div class="flex gap-4 border-r-red-300">
+        <a class="underline" href="/">Red</a>
+        <a class="underline" href="/">White</a>
+      </div>
+      <div>
+        <a class="underline" href="/">Menu</a>
+      </div>
+    </div>
+  </div>
+</header>
