9c1d2b84876aac74a08478964fe553126fa82ecb
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-22 20:36:23 -0300

    refactor: grid-home - componentizes first section

---
+++ src/components/first_section_home.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import { chapters } from "@/lib/assets/grid_home";
+
+  let currentIndex: number | null = null;
+
+  function handleIndex(index: number) {
+    currentIndex = index;
+  }
+
+  function handleLeave() {
+    currentIndex = null;
+  }
+</script>
+
+<div class="w-svw grid h-[100vh] p-6 pt-0 grid-rows-home_rows justify-center">
+  <div></div>
+  <div class="grid grid-cols-4 justify-center">
+    {#each chapters as chapter, index}
+      <div
+        id="column"
+        aria-hidden="true"
+        on:mouseleave={handleLeave}
+        on:mouseenter={handleIndex.bind(null, index)}
+        class="opacity-0 grid cursor-pointer"
+      >
+        <div
+          class:first={index === 0}
+          class:last={index === 3}
+          class="p-3 h-full grid grid-rows-column_row text-center"
+        >
+          <div
+            class="w-full relative overflow-hidden cursor-pointer object-cover h-full self-end"
+          >
+            <img
+              alt={chapter.description}
+              src={chapter.src}
+              class="w-full absolute left-0 right-0 bottom-0 top-0 h-full select-none object-cover"
+            />
+          </div>
+          <h2
+            class:active={index === currentIndex}
+            class:off={currentIndex !== null && currentIndex !== index}
+            class="text-hells_red leading-7 transition-all pt-2 flex flex-col font-futura_lt_bold text-[40px] uppercase"
+          >
+            <span class="text-[12px] font-normal font-futura_bt"
+              >{chapter.chapter}</span
+            >
+            {chapter.title}
+          </h2>
+        </div>
+      </div>
+    {/each}
+  </div>
+  <img
+    class="w-[98vw] h-full pt-6 bottom-0 self-end"
+    src="/logo/logo.svg"
+    alt=""
+  />
+</div>
+
+<style>
+  .active {
+    @apply transition-all;
+  }
+
+  .off {
+    @apply opacity-100 transition-all brightness-50;
+  }
+
+  .first {
+    @apply pl-0 text-start;
+  }
+
+  .last {
+    @apply pr-0 text-end;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,19 +9,9 @@
   import { PreloadTimeline } from "@/lib/animations/timelines/PreloadTimeline";
   import { SkipPreloadTimeline } from "@/lib/animations/timelines/SkipPreloadTimeline";
   import { chapters } from "@/lib/assets/grid_home";
+  import FirstSectionHome from "@/components/first_section_home.svelte";
 
   let timeline: GSAPTimeline;
-
-
-  let currentIndex: number | null = null;
-
-  function handleIndex(index: number) {
-    currentIndex = index;
-  }
-
-  function handleLeave() {
-    currentIndex = null;
-  }
 
   onMount(() => {
     timeline = new PreloadTimeline(false).getTimeline();
@@ -38,55 +28,11 @@
   }
 </script>
 
-<div class="h-dvh relative grid items-end justify-center">
+<div class="h-dvh relative grid justify-center">
   {#if !$preloadFinished}
     <Preload on:skipToEnd={skipToEnd} />
   {/if}
-  <div class="w-svw grid h-[100vh] p-6 pt-0 grid-rows-home_rows justify-center">
-    <div></div>
-    <div class="grid grid-cols-4 justify-center">
-      {#each chapters as chapter, index}
-        <div
-          id="column"
-          aria-hidden="true"
-          on:mouseleave={handleLeave}
-          on:mouseenter={handleIndex.bind(null, index)}
-          class="opacity-0 grid cursor-pointer"
-        >
-          <div
-            class:first={index === 0}
-            class:last={index === 3}
-            class="p-3 h-full grid grid-rows-column_row text-center"
-          >
-            <div
-              class="w-full relative overflow-hidden cursor-pointer object-cover h-full self-end"
-            >
-              <img
-                alt={chapter.description}
-                src={chapter.src}
-                class="w-full absolute left-0 right-0 bottom-0 top-0 h-full select-none object-cover"
-              />
-            </div>
-            <h2
-              class:active={index === currentIndex}
-              class:off={currentIndex !== null && currentIndex !== index}
-              class="text-hells_red leading-7 transition-all pt-2 flex flex-col font-futura_lt_bold text-[40px] uppercase"
-            >
-              <span class="text-[12px] font-normal font-futura_bt"
-                >{chapter.chapter}</span
-              >
-              {chapter.title}
-            </h2>
-          </div>
-        </div>
-      {/each}
-    </div>
-    <img
-      class="w-[98vw] h-full pt-6 bottom-0 self-end"
-      src="/logo/logo.svg"
-      alt=""
-    />
-  </div>
+  <FirstSectionHome />
 </div>
 <div class="h-[100vh] w-[100vw] relative p-6 grid items-end text-hells_red">
   <div class="text-center grid justify-center font-century">
