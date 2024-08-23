a72797f43bf815e7ca8d70e07682027c2e793125
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-23 14:17:51 -0300

    feat: chapters- add routes

--- src/lib/assets/grid_home.ts
+++ src/lib/assets/grid_home.ts
@@ -1,26 +1,30 @@
 export const chapters = [
-    {
-      title: "Becoming",
-      description: "The Ferrari World",
-      src: "/images_grid/grid_one.webp",
-      chapter: "CHapter One",
-    },
-    {
-      title: "Making",
-      description: "The Ferrari World",
-      src: "/images_grid/grid_two.webp",
-      chapter: "CHapter Two",
-    },
-    {
-      title: "The Stories Of",
-      description: "The Ferrari World",
-      src: "/images_grid/grid_three.webp",
-      chapter: "CHapter Three",
-    },
-    {
-      title: "Gallery",
-      description: "The Ferrari World",
-      src: "/images_grid/grid_four.webp",
-      chapter: "View",
-    },
-  ];
+  {
+    title: "Becoming",
+    description: "Becoming Ferrari",
+    src: "/images_grid/grid_one.webp",
+    chapter: "Chapter One",
+    href: "/becoming",
+  },
+  {
+    title: "Making",
+    description: "Making Ferrari",
+    src: "/images_grid/grid_two.webp",
+    chapter: "Chapter Two",
+    href: "/making",
+  },
+  {
+    title: "The Stories Of",
+    description: "The Stories Of Ferrari",
+    src: "/images_grid/grid_three.webp",
+    chapter: "Chapter Three",
+    href: "/stories",
+  },
+  {
+    title: "Gallery",
+    description: "Gallery",
+    src: "/images_grid/grid_four.webp",
+    chapter: "View",
+    href: "/becoming",
+  },
+];
