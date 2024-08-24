5d013bf1aa37c82d6de8364b1d17ca0f35ba2d4f
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-24 17:16:01 -0300

    refactor: helpers - add helpers functions

--- src/lib/helpers/preload.ts
+++ src/lib/helpers/preload.ts
@@ -1,4 +1,7 @@
 import gsap from "gsap";
+import { pauseVideo, startVideo, preloadFinished } from "../stores/preload";
+
+// -- Events -- //
 
 function preventScroll(event: WheelEvent | TouchEvent) {
   event.preventDefault();
@@ -14,9 +17,86 @@ export function unlockScroll() {
   document.removeEventListener("touchmove", preventScroll);
 }
 
+// -- Animations -- //
+
 export function showColumns() {
   gsap.to("#column", {
     opacity: 1,
     stagger: 0.5,
   });
+}
+
+// -- Timelines -- //
+
+export function preloadTimeline() {
+  return gsap
+    .timeline()
+    .to(
+      "#container_preload__logo",
+      {
+        width: "90%",
+        ease: "power1.inOut",
+        duration: 7,
+        onStart: () => {
+          lockScroll();
+        },
+      },
+      1.1,
+    )
+    .to(
+      "#container_preload__video",
+      {
+        opacity: 1,
+        duration: 2,
+        onStart: () => {
+          startVideo.set(true);
+        },
+      },
+      1.1,
+    )
+    .to(
+      "#container_preload__video",
+      {
+        opacity: 0,
+        duration: 0.4,
+      },
+      7.6,
+    )
+    .to(
+      "#container_preload__container_copy_click",
+      {
+        opacity: 0,
+        duration: 0.4,
+      },
+      7.4,
+    );
+}
+
+export function skipPreload(): GSAPTimeline {
+  return gsap
+    .timeline()
+    .to("#container_preload__video", {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {
+        pauseVideo.set(true);
+      },
+    })
+    .to(
+      "#container_preload__container_copy_click",
+      {
+        opacity: 0,
+        duration: 0.4,
+      },
+      0.15,
+    );
+}
+
+// -- Functions -- //
+
+export function completeActions() {
+  preloadFinished.set(true);
+  pauseVideo.set(true);
+  unlockScroll();
+  showColumns();
 }
