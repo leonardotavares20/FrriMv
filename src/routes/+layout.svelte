fb07e4e16aaf03bb66b23d7f45ab12fa3f8432d6
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-24 18:07:02 -0300

    refactor: preload - pass route parameters to preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
   import "../app.css";
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import Header from "@/components/header.svelte";
   import { afterNavigate } from "$app/navigation";
   import Preload from "@/components/preload.svelte";
   import { preloadFinished, firstLoad } from "@/lib/stores/preload";
   import { PreloadTimeline } from "@/lib/animations/timelines/PreloadTimeline";
   import { SkipPreloadTimeline } from "@/lib/animations/timelines/SkipPreloadTimeline";
+  import { get } from "svelte/store";
+  import { page } from "$app/stores";
 
   let timeline: GSAPTimeline;
 
   $: $preloadFinished && firstLoad.set(true);
 
+  function inHome() {
+    const path = get(page);
+    return path.url.pathname === "/";
+  }
+
   function skipPreloadToEnd() {
     timeline.pause();
 
-    const skipTimeline = new SkipPreloadTimeline(false).getTimeline();
+    const skipTimeline = new SkipPreloadTimeline(false, inHome()).getTimeline();
 
     skipTimeline.eventCallback("onComplete", () => {
       timeline.seek(timeline.duration());
@@ -26,7 +33,7 @@
 
   onMount(() => {
     if (!$preloadFinished && !$firstLoad) {
-      timeline = new PreloadTimeline(false).getTimeline();
+      timeline = new PreloadTimeline(false, inHome()).getTimeline();
     }
   });
 
@@ -39,11 +46,11 @@
   <title>Ferrari - Movie</title>
 </svelte:head>
 
-{#if !$preloadFinished && !$firstLoad}
-  <Preload on:skipToEnd={skipPreloadToEnd} />
-{/if}
 <div class="app">
   <Header />
+  {#if !$preloadFinished && !$firstLoad}
+    <Preload on:skipToEnd={skipPreloadToEnd} />
+  {/if}
   <main>
     <slot></slot>
   </main>
