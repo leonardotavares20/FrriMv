0b8adc7bbea4bbd864b9bc0b2a425c24efce3d77
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-24 02:18:38 -0300

    feat: becoming - adjust column title page

--- src/routes/becoming/+page.svelte
+++ src/routes/becoming/+page.svelte
@@ -9,7 +9,7 @@
 
   afterNavigate(() => {
     return new Promise((res) => {
-      gsap.to("#first", {
+      gsap.to("#column_one", {
         opacity: 1,
         delay: 0.5,
       });
@@ -19,20 +19,31 @@
   onMount(() => {
     let timeline = gsap.timeline();
 
-    timeline.to("#first", {
-      opacity: 1,
-      ease: "power2.inOut",
-      duration: 1.2,
-      translateX: "0%",
-    });
-    timeline.to(
-      "#cover",
-      {
-        opacity: 0,
-        duration: 0.4,
-      },
-      0.5,
-    );
+    timeline
+      .to("#column_one", {
+        opacity: 1,
+        ease: "power2.inOut",
+        duration: 1.2,
+        translateX: "0%",
+      })
+      .to(
+        "#column_two",
+        {
+          opacity: 1,
+          ease: "power2.inOut",
+          duration: 1.2,
+          translateX: "0%",
+        },
+        0,
+      )
+      .to(
+        "#cover",
+        {
+          opacity: 0,
+          duration: 0.4,
+        },
+        0.5,
+      );
   });
 
   onNavigate(async () => {
@@ -45,7 +56,7 @@
 <div bind:this={becoming}>
   <div class="h-[100vh] grid grid-cols-2">
     <div
-      id="first"
+      id="column_one"
       class="object-cover opacity-0 -translate-x-full h-[100vh] relative"
     >
       <Cover />
@@ -55,10 +66,17 @@
         class="h-[100vh] w-full z-10 relative object-cover"
       />
     </div>
-    <div class="text-hells_red">
-      <h2>
-        <span> Adam Driver, Penélope Cruz </span>
-        Becoming Ferrari
+    <div
+      id="column_two"
+      class="text-hells_red grid justify-center items-end translate-x-full opacity-0"
+    >
+      <h2
+        class="text-center font-futura_lt text-[140px] uppercase leading-[125px] pb-7"
+      >
+        <span class=" text-xs font-futura_bt">
+          Adam Driver, Penélope Cruz
+        </span> <br />
+        Becoming <br /> Ferrari
       </h2>
     </div>
   </div>
