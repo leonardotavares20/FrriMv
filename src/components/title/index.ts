cf3fccb6d06c4a91db89e14aacc5c9e8e97c7487
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 01:05:11 -0300

    feat: components - add title component

---
+++ src/components/title/index.ts
@@ -0,0 +1,9 @@
+import TitleContainer from "./title_container.svelte";
+import TitleLg from "./title_lg.svelte";
+import TitleSm from "./title_sm.svelte";
+
+export const Title = {
+  TitleContainer: TitleContainer,
+  TitleLg: TitleLg,
+  TitleSm: TitleSm,
+};
