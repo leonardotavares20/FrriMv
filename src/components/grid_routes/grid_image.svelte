1bc575ebd6bceaa8714c60c937cd1bb1d0ce744c
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 15:36:55 -0300

    feat: component - add grid image

---
+++ src/components/grid_routes/grid_image.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+</script>
+
+<img class="h-[100vh] w-full z-10 relative object-cover" {src} {alt} />
--- src/components/grid_routes/index.ts
+++ src/components/grid_routes/index.ts
@@ -1,9 +1,11 @@
 import GridColumns from "./grid_columns.svelte";
 import GridColumnOne from "./grid_column_one.svelte";
 import GridColumnTwo from "./grid_column_two.svelte";
+import GridImage from "./grid_image.svelte";
 
 export const Grid = {
   GridColumns: GridColumns,
+  GridImage: GridImage,
   ColumnOne: GridColumnOne,
   ColumnTwo: GridColumnTwo,
 };
