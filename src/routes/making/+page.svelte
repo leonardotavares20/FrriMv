c5489d93ce84c11e8e0759e1c1a7f8dcb9ed0d73
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 15:37:57 -0300

    feat: making - add making grid

--- src/routes/making/+page.svelte
+++ src/routes/making/+page.svelte
@@ -1,1 +1,41 @@
-<p class="bg-slate-200">oi</p>
+<script lang="ts">
+  import NavigateTransition from "@/components/transitions/navigate_transition.svelte";
+  import Cover from "@/components/cover/cover.svelte";
+  import { Grid } from "@/components/grid_routes";
+  import PageWrapper from "@/components/page_wrapper/page_wrapper.svelte";
+  import { Title } from "@/components/title";
+  import { afterNavigate } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { showColumnOneGrid } from "@/lib/helpers/grid";
+  import { GridRoutesTimeline } from "@/lib/animations/timelines/GridRoutesTimeline";
+
+  afterNavigate(() => {
+    return new Promise(() => {
+      showColumnOneGrid();
+    });
+  });
+
+  onMount(() => {
+    new GridRoutesTimeline(false).getTimeline().play();
+  });
+</script>
+
+<NavigateTransition>
+  <PageWrapper>
+    <Grid.GridColumns>
+      <Grid.ColumnOne>
+        <Cover />
+        <Grid.GridImage
+          src="/images_grid/grid_two.webp"
+          alt="Behind the scenes of the filming of the movie Ferrari"
+        />
+      </Grid.ColumnOne>
+      <Grid.ColumnTwo>
+        <Title.TitleContainer>
+          <Title.TitleSm>Chapter Two</Title.TitleSm>
+          <Title.TitleLg>Making Ferrari</Title.TitleLg>
+        </Title.TitleContainer>
+      </Grid.ColumnTwo>
+    </Grid.GridColumns>
+  </PageWrapper>
+</NavigateTransition>
