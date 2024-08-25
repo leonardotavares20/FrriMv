5f62b4a517ed6b35ab58358af3bcf1436400458c
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 01:09:34 -0300

    feat: grid - add grid component for routes

---
+++ src/components/grid_routes/grid_column_one.svelte
@@ -0,0 +1,6 @@
+<div
+  id="column_one"
+  class="object-cover opacity-0 -translate-x-full h-[100vh] relative"
+>
+  <slot />
+</div>
---
+++ src/components/grid_routes/index.ts
@@ -0,0 +1,9 @@
+import GridColumns from "./grid_columns.svelte";
+import GridColumnOne from "./grid_column_one.svelte";
+import GridColumnTwo from "./grid_column_two.svelte";
+
+export const Grid = {
+  GridColumns: GridColumns,
+  ColumnOne: GridColumnOne,
+  ColumnTwo: GridColumnTwo,
+};
