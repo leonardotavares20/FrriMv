f66134ec68c96c0600c735d0864c4c0d0daccf2d
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 22:34:35 -0300

    feat: stories - add hero section

--- src/routes/stories/+page.svelte
+++ src/routes/stories/+page.svelte
@@ -1,1 +1,41 @@
-<p class="bg-slate-200">oi</p>
+<script lang="ts">
+  import NavigateTransition from "@/components/transitions/navigate_transition.svelte";
+  import Cover from "@/components/cover/cover.svelte";
+  import { Grid } from "@/components/grid_routes";
+  import PageWrapper from "@/components/page_wrapper/page_wrapper.svelte";
+  import { Title } from "@/components/title";
+  import { afterNavigate } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { showColumnOneGrid } from "@/lib/helpers/grid";
+  import { GridRoutesTimeline } from "@/lib/animations/timelines/GridRoutesTimeline";
+
+  afterNavigate(() => {
+    return new Promise(() => {
+      showColumnOneGrid();
+    });
+  });
+
+  onMount(() => {
+    new GridRoutesTimeline(false).getTimeline().play();
+  });
+</script>
+
+<NavigateTransition>
+  <PageWrapper>
+    <Grid.GridColumns>
+      <Grid.HeroColumnOne>
+        <Cover />
+        <Grid.GridImage
+          src="/images_grid/grid_three.webp"
+          alt="Penelope Cruz walking towards the camera in a grey dress"
+        />
+      </Grid.HeroColumnOne>
+      <Grid.HeroColumnTwo>
+        <Title.TitleContainer>
+          <Title.TitleSm>Chapter Three</Title.TitleSm>
+          <Title.TitleLg>The Stories Of <br /> Ferrari</Title.TitleLg>
+        </Title.TitleContainer>
+      </Grid.HeroColumnTwo>
+    </Grid.GridColumns>
+  </PageWrapper>
+</NavigateTransition>
