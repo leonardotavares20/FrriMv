95613e5840bb11620c13ff0f049e09b6c1805d5c
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 22:42:57 -0300

    refactor: stories - add hero component

---
+++ src/routes/stories/hero.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Title } from "@/components/title";
+  import { Grid } from "@/components/grid_routes";
+  import Cover from "@/components/cover/cover.svelte";
+</script>
+
+<Grid.GridColumns>
+  <Grid.HeroColumnOne>
+    <Cover />
+    <Grid.GridImage
+      src="/images_grid/grid_three.webp"
+      alt="Penelope Cruz walking towards the camera in a grey dress"
+    />
+  </Grid.HeroColumnOne>
+  <Grid.HeroColumnTwo>
+    <Title.TitleContainer>
+      <Title.TitleSm>Chapter Three</Title.TitleSm>
+      <Title.TitleLg>The Stories Of <br /> Ferrari</Title.TitleLg>
+    </Title.TitleContainer>
+  </Grid.HeroColumnTwo>
+</Grid.GridColumns>
