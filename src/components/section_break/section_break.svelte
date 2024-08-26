748b0d6cc1f602045d40e8ec7cc566ee6aae1456
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-25 22:31:19 -0300

    feat: components - add section-break component

---
+++ src/components/section_break/section_break.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div class="grid font-futura_lt pt-40 px-5 pb-3 items-end text-hells_red">
+  <div>
+    <div class="w-full"><p>{title}</p></div>
+    <div class="bg-hells_red h-2"></div>
+  </div>
+</div>
