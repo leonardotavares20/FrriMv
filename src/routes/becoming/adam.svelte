34f1fb422e419a853c3afc16d1ad59bdcff31127
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-29 02:16:00 -0300

    feat: becoming-adam-section - add video preview to section

--- src/routes/becoming/adam.svelte
+++ src/routes/becoming/adam.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import SectionBreak from "@/components/section_break/section_break.svelte";
+  import VideoPreview from "@/components/video/video_preview.svelte";
 </script>
 
 <SectionBreak title="Adam Driver" />
+<VideoPreview srcVideo="/videos/becoming/adam/adam.mp4" />
