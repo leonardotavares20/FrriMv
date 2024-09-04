f72f5b5eb95fa60d9e099763122c0c2d2cb92f68
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 18:40:06 -0300

    feat: stories - add mille miglia description

---
+++ src/routes/stories/mille_miglia_description.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Title } from "@/components/title";
+  import SectionDescription from "@/components/section/section_description.svelte";
+</script>
+
+<section
+  class="w-full flex flex-col justify-center items-center grid-rows-1 h-[100vh]"
+>
+  <Title.TitleContainer>
+    <Title.TitleLg>THE MILLE <br /> MIGLIA</Title.TitleLg>
+  </Title.TitleContainer>
+  <SectionDescription>
+    As Enzo Ferrari saw it, he was in the business of selling cars in order to
+    fuel his passion for racing. Those two vectors collided in 1957 with Enzo on
+    the verge of insolvency. His solution: win the Mille Miglia (Thousand
+    Miles), Italy's preeminent endurance race during the first half of the 20th
+    Century. With the fame and glory and attention of such a victory would come
+    increased sales, and as a result, make Ferrari attractive to financiers on
+    his terms. The Mille Miglia was a dangerous race in a dangerous era of
+    racing. But the focus required makes the act of racing something akin to
+    meditation, in the estimation of actor Patrick Dempsey, who stars as 1957
+    Mille Migila victor Piero Tarrufi and who has earned his share of podium
+    finishes as a driver himself. With potential dangers lurking at every turn,
+    there's a cleansing process of the mind where all extraneous thought
+    recedes.
+  </SectionDescription>
+</section>
