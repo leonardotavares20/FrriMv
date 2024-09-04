78f189cd0cd2bf4ce9e002727e5b12bf9dffb1c0
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 01:08:32 -0300

    content: assets - add asset adam scroll

---
+++ src/lib/assets/becoming/adam_scroll.ts
@@ -0,0 +1,6 @@
+export const adam_images = [
+  { id: 1, src: "becoming/first_carrousel/first.webp", alt: "Adam" },
+  { id: 2, src: "becoming/first_carrousel/second.webp", alt: "Adam" },
+  { id: 3, src: "becoming/first_carrousel/third.webp", alt: "Adam" },
+  { id: 4, src: "becoming/first_carrousel/fourth.webp", alt: "Adam" },
+];
