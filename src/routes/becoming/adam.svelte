869676fcfcdbffd300394b6e60b458bcf1a7ae38
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 01:23:21 -0300

    feat: becoming - add finished sections

--- src/routes/becoming/adam.svelte
+++ src/routes/becoming/adam.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-  import SectionBreak from "@/components/section_break/section_break.svelte";
+  import AdamColumns from "./adam_columns.svelte";
+  import { adam_images } from "@/lib/assets/becoming/adam_scroll";
+  import AdamHorizontalScroll from "./adam_horizontal_scroll.svelte";
   import VideoPreview from "@/components/video/video_preview.svelte";
+  import SectionBreak from "@/components/section/section_break.svelte";
+  import AdamSectionDescription from "./adam_section_description.svelte";
 </script>
 
-<SectionBreak title="Adam Driver" />
-<VideoPreview srcVideo="/videos/becoming/adam/adam.mp4" />
+<section id="adam_section">
+  <SectionBreak title="Adam Driver" />
+  <VideoPreview srcVideo="/videos/becoming/adam/adam.mp4" />;
+  <AdamSectionDescription />
+  <AdamHorizontalScroll images={adam_images} />
+  <AdamColumns />
+</section>
