67d6d5dcef221892284bf86bf3a6c1e1e2e9c255
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 01:20:34 -0300

    feat: component - add scroll message

---
+++ src/components/horizontal_scroll/horizontal_scroll_message.svelte
@@ -0,0 +1,5 @@
+<span
+  id="horizontal_scroll_message"
+  class="text-hells_red uppercase absolute left-1/2 bottom-20 font-futura_lt text-xs -translate-x-1/2 opacity-0"
+  >Continue Scrolling</span
+>
