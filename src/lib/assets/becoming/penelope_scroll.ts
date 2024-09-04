31ed9532d925d5ecd3a2d9a73a494de899c24607
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 01:08:43 -0300

    content: assets - add asset penelope scroll

---
+++ src/lib/assets/becoming/penelope_scroll.ts
@@ -0,0 +1,6 @@
+export const penelope_images = [
+  { id: 1, src: "becoming/second_carrousel/first.webp", alt: "Penelope" },
+  { id: 2, src: "becoming/second_carrousel/second.webp", alt: "Penelope" },
+  { id: 3, src: "becoming/second_carrousel/third.webp", alt: "Penelope" },
+  { id: 4, src: "becoming/second_carrousel/fourth.webp", alt: "Penelope" },
+];
