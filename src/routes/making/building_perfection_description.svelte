0956a0519086d63b14d8f7992d4e36241f97bf7b
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 11:09:38 -0300

    feat: making - building perfect section description

---
+++ src/routes/making/building_perfection_description.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Title } from "@/components/title";
+  import SectionDescription from "@/components/section/section_description.svelte";
+</script>
+
+<section
+  class="w-full flex flex-col justify-center items-center grid-rows-1 h-[100vh]"
+>
+  <Title.TitleContainer>
+    <Title.TitleLg>Building <br /> Perfection</Title.TitleLg>
+  </Title.TitleContainer>
+  <SectionDescription>
+    In life, when a thing works better, usually it is more beautiful to the eye.
+    That's how Enzo Ferrari puts it to his son Piero in a scene from FERRARI.
+    Director Michael Mann often calls Ferrari race cars moving sculpture, while
+    noting the irony that they are not self-consciously designed as such. So,
+    all the technological innovation and Italian intuition that goes into their
+    design and manufacture became another area of exploration, as Mann tasked
+    his team with creating a fleet of race cars from the ground up. Using
+    specially designed chassis, 3D LiDAR scans of actual race cars — which of
+    course couldn't be used in the film themselves, given their extraordinary
+    value — and hand-crafted metal and fiberglass sculpts, the production
+    succeeded in replicating the cars with a blend of old-world techniques and
+    modern-day technology. Once they were painted and camera-ready, actors
+    Patrick Dempsey (Piero Taruffi), Gabriel Leone (Alfonso De Portago) and Jack
+    O'Connell (Peter Collins) could have their fun. Real-life racing drivers
+    including actor Patrick Dempsey (Piero Taruffi), Derek Hill and Marino
+    Franchitti also star as Jean Behra and Eugenio Castellotti, respectively,
+    two celebrated drivers of the 1950s.
+  </SectionDescription>
+</section>
