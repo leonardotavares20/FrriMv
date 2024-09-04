4e2f15d7cdb963ba05681494d70e5590c3eadf2a
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 01:12:20 -0300

    feat: component - add carrousel component

---
+++ src/components/carrousel/index.ts
@@ -0,0 +1,7 @@
+import CarrouselSlider from "./carrousel_slider.svelte";
+import CarrouselContainer from "./carrousel_container.svelte";
+
+export const Carrousel = {
+  CarrouselSlider: CarrouselSlider,
+  CarrouselContainer: CarrouselContainer,
+};
