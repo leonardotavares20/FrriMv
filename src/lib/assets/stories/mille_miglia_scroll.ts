6ce5e5f1ef07550937f8ab9dc5111484b417d5ce
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 18:39:28 -0300

    content: assets - add mille miglia scroll images

---
+++ src/lib/assets/stories/mille_miglia_scroll.ts
@@ -0,0 +1,50 @@
+export const mille_miglia_images = [
+  {
+    id: 1,
+    src: "stories/carrousel/first.webp",
+    alt: "Mille Miglia",
+    full: false,
+  },
+  {
+    id: 2,
+    src: "stories/carrousel/second.webp",
+    alt: "Mille Miglia",
+    full: true,
+  },
+  {
+    id: 3,
+    src: "stories/carrousel/third.webp",
+    alt: "Mille Miglia",
+    full: false,
+  },
+  {
+    id: 4,
+    src: "stories/carrousel/fourth.webp",
+    alt: "Mille Miglia",
+    full: true,
+  },
+  {
+    id: 5,
+    src: "stories/carrousel/fifth.webp",
+    alt: "Mille Miglia",
+    full: false,
+  },
+  {
+    id: 6,
+    src: "stories/carrousel/sixth.webp",
+    alt: "Mille Miglia",
+    full: false,
+  },
+  {
+    id: 7,
+    src: "stories/carrousel/seventh.webp",
+    alt: "Mille Miglia",
+    full: true,
+  },
+  {
+    id: 8,
+    src: "stories/carrousel/eighth.webp",
+    alt: "Mille Miglia",
+    full: false,
+  },
+];
