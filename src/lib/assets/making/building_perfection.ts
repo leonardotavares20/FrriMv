1724990b656d161c5101009aeff353322aa65849
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-04 11:05:14 -0300

    content: building-perfection - add asset

---
+++ src/lib/assets/making/building_perfection.ts
@@ -0,0 +1,6 @@
+export const building_perfection_images = [
+  { id: 1, src: "making/second_carrousel/first.webp", alt: "Chapter One" },
+  { id: 2, src: "making/second_carrousel/second.webp", alt: "Chapter One" },
+  { id: 3, src: "making/second_carrousel/third.webp", alt: "Chapter One" },
+  { id: 4, src: "making/second_carrousel/fourth.webp", alt: "Chapter One" },
+];
