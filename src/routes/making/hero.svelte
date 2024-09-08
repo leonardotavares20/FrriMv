00c923e274dbd039ab113ec07e20c0fc61c0e073
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-08 18:47:16 -0300

    refactor: making - implements wrapper image and remove cover

--- src/routes/making/hero.svelte
+++ src/routes/making/hero.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
   import { Title } from "@/components/title";
   import { Grid } from "@/components/grid_routes";
-  import Cover from "@/components/cover/cover.svelte";
+  import ImageWrapper from "@/components/wrappers/image_wrapper.svelte";
 </script>
 
 <Grid.GridColumns>
   <Grid.HeroColumnOne>
-    <Cover />
-    <Grid.GridImage
-      src="/images_grid/grid_two.webp"
-      alt="Behind the scenes of the filming of the movie Ferrari"
-    />
+    <ImageWrapper sourceImage="/images_grid/grid_two.webp">
+      <Grid.GridImage
+        src="/images_grid/grid_two.webp"
+        alt="Behind the scenes of the filming of the movie Ferrari"
+      />
+    </ImageWrapper>
   </Grid.HeroColumnOne>
   <Grid.HeroColumnTwo>
     <Title.TitleContainer>
       <Title.TitleSm>Chapter Two</Title.TitleSm>
-      <Title.TitleLg>Making <br> Ferrari</Title.TitleLg>
+      <Title.TitleLg>Making <br /> Ferrari</Title.TitleLg>
     </Title.TitleContainer>
   </Grid.HeroColumnTwo>
 </Grid.GridColumns>
