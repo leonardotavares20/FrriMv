8c9a87070b718a5dc02fb19dff2d09fafcc4321c
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-14 05:06:50 -0300

    fix: making-hero - remove image wrapper and pass index

--- src/routes/making/hero.svelte
+++ src/routes/making/hero.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
   import { Title } from "@/components/title";
   import { Grid } from "@/components/grid_routes";
-  import ImageWrapper from "@/components/wrappers/image_wrapper.svelte";
 </script>
 
 <Grid.GridColumns>
   <Grid.HeroColumnOne>
-    <ImageWrapper sourceImage="/images_grid/grid_two.webp">
-      <Grid.GridImage
-        src="/images_grid/grid_two.webp"
-        alt="Behind the scenes of the filming of the movie Ferrari"
-      />
-    </ImageWrapper>
+    <Grid.GridImage
+      indexImage={2}
+      src="/images_grid/grid_two.webp"
+      alt="Behind the scenes of the filming of the movie Ferrari"
+    />
   </Grid.HeroColumnOne>
   <Grid.HeroColumnTwo>
     <Title.TitleContainer>
