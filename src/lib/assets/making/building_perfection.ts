8ea03afdc27964c4182e77c7ebaa89716b4f8f97
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-14 04:58:23 -0300

    feat: building-perfection-assets - add index to obj

--- src/lib/assets/making/building_perfection.ts
+++ src/lib/assets/making/building_perfection.ts
@@ -1,6 +1,26 @@
 export const building_perfection_images = [
-  { id: 1, src: "making/second_carrousel/first.webp", alt: "Chapter One" },
-  { id: 2, src: "making/second_carrousel/second.webp", alt: "Chapter One" },
-  { id: 3, src: "making/second_carrousel/third.webp", alt: "Chapter One" },
-  { id: 4, src: "making/second_carrousel/fourth.webp", alt: "Chapter One" },
+  {
+    id: 1,
+    src: "making/second_carrousel/first.webp",
+    alt: "Chapter One",
+    indexImage: 25,
+  },
+  {
+    id: 2,
+    src: "making/second_carrousel/second.webp",
+    alt: "Chapter One",
+    indexImage: 26,
+  },
+  {
+    id: 3,
+    src: "making/second_carrousel/third.webp",
+    alt: "Chapter One",
+    indexImage: 27,
+  },
+  {
+    id: 4,
+    src: "making/second_carrousel/fourth.webp",
+    alt: "Chapter One",
+    indexImage: 28,
+  },
 ];
