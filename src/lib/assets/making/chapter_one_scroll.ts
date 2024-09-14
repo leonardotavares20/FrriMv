d041b2b65a4e9bb25e736ab5f98aba75c509f4b7
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-14 04:58:34 -0300

    feat: chaoter-one-assets - add index to obj

--- src/lib/assets/making/chapter_one_scroll.ts
+++ src/lib/assets/making/chapter_one_scroll.ts
@@ -1,6 +1,26 @@
 export const chapter_one_images = [
-  { id: 1, src: "making/first_carrousel/first.webp", alt: "Chapter One" },
-  { id: 2, src: "making/first_carrousel/second.webp", alt: "Chapter One" },
-  { id: 3, src: "making/first_carrousel/third.webp", alt: "Chapter One" },
-  { id: 4, src: "making/first_carrousel/fourth.webp", alt: "Chapter One" },
+  {
+    id: 1,
+    src: "making/first_carrousel/first.webp",
+    alt: "Chapter One",
+    indexImage: 17,
+  },
+  {
+    id: 2,
+    src: "making/first_carrousel/second.webp",
+    alt: "Chapter One",
+    indexImage: 18,
+  },
+  {
+    id: 3,
+    src: "making/first_carrousel/third.webp",
+    alt: "Chapter One",
+    indexImage: 19,
+  },
+  {
+    id: 4,
+    src: "making/first_carrousel/fourth.webp",
+    alt: "Chapter One",
+    indexImage: 20,
+  },
 ];
