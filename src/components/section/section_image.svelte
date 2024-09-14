37735322a8b38289452ee2371d9ce736d8e3685f
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-14 04:55:13 -0300

    refactor: section-image - implements image wrapper

--- src/components/section/section_image.svelte
+++ src/components/section/section_image.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   export let src: string;
   export let alt: string;
+  export let indexImage: number;
   import ImageWrapper from "../wrappers/image_wrapper.svelte";
 </script>
 
-<ImageWrapper sourceImage={src}>
+<ImageWrapper {indexImage} sourceImage={src}>
   <div class="w-[100vw] h-[130vh] min-h-[500px] pb-20 mb-20">
     <img class="w-[98vw] object-cover m-auto h-[130vh]" {src} {alt} />
   </div>
