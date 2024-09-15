c818de39e8a7aba21a2fb72062efdbcaa90cfbef
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-15 15:42:02 -0300

    feat: menu-assets - add menu asset

---
+++ src/lib/assets/menu/menu.ts
@@ -0,0 +1,8 @@
+export const menu = [
+  { text: "Becoming Ferrari", first: true, href: "/becoming" },
+  { text: "Making Ferrari", first: false, href: "/making" },
+  { text: "The Stories of Ferrari", first: false, href: "/stories" },
+  { text: "Gallery", first: false, href: "/gallery" },
+  { text: "Watch Trailer", first: false, href: "/trailer" },
+  { text: "Get Tickets", first: false, href: "/tickets" },
+];
