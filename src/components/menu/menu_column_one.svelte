8c66ac545acc4f1ea553e54c05f28dd16597d038
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-15 15:43:47 -0300

    feat: menu-grid - add menu grid column one

---
+++ src/components/menu/menu_column_one.svelte
@@ -0,0 +1,24 @@
+<div id="menu_column_one" class="h-svh relative bg-black -translate-x-full">
+  <div class="w-full h-full absolute top-0 bottom-0 left-0 right-0">
+    <div>
+      <img
+        class="absolute w-full h-full top-0 left-0 right-0 bottom-0 object-cover"
+        src="/images_grid/grid_one.webp"
+        alt=""
+      />
+      <div
+        class="absolute top-1/2 grid justify-items-center gap-4 -translate-y-1/2"
+      >
+        <img
+          id="logo_ferrari"
+          class="w-1/2"
+          src="/logo/logo.svg"
+          alt="Ferrari"
+        />
+        <p class="uppercase font-futura_bt text-2xl text-hells_red">
+          In theaters christmas
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
