9a98d1c514026995f0ffe4c9557307e0b5706d65
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-15 16:16:17 -0300

    fix: menu-assets - remove gallery asset

--- src/lib/assets/menu/menu.ts
+++ src/lib/assets/menu/menu.ts
@@ -2,7 +2,7 @@ export const menu = [
   { text: "Becoming Ferrari", first: true, href: "/becoming" },
   { text: "Making Ferrari", first: false, href: "/making" },
   { text: "The Stories of Ferrari", first: false, href: "/stories" },
-  { text: "Gallery", first: false, href: "/gallery" },
+  { text: "Gallery", first: false, href: "" },
   { text: "Watch Trailer", first: false, href: "/trailer" },
   { text: "Get Tickets", first: false, href: "/tickets" },
 ];
