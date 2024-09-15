060057ca47a96ca81bcc8c3e4f53bd12fe9874b3
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-15 15:44:13 -0300

    feat: menu-grid - add menu grid

---
+++ src/components/menu/menu_grid.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import MenuColumnOne from "./menu_column_one.svelte";
+  import MenuColumnTwo from "./menu_column_two.svelte";
+</script>
+
+<div id="menu" class="w-svw h-svh grid grid-cols-2 fixed z-[100] pointer-events-none">
+  <MenuColumnOne />
+  <MenuColumnTwo />
+</div>
