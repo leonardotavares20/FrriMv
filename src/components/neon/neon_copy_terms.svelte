4c3a5e463b98dad2887cf1b713a2c61bd4a6cd04
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-09-15 15:43:07 -0300

    refactor: neon-copy-logo - add style condition

--- src/components/neon/neon_copy_terms.svelte
+++ src/components/neon/neon_copy_terms.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
   import NeonLogo from "@/components/neon/neon_logo.svelte";
+  export let start: boolean = false;
 </script>
 
 <div
   class="uppercase w-24 font-futura_bt text-hells_red text-center font-semibold"
 >
-  <span class="flex justify-center text-xs gap-1">
+  <span class="flex justify-center text-xs gap-1" class:start>
     A <NeonLogo /> Movie
   </span>
-  <p class="text-[8px] leading-[8px] mt-1 font-semibold">
+  <p class="text-[8px] leading-[8px] mt-1 font-semibold" class:start>
     Copyrights © Neon 2023
   </p>
-  <p class="text-[8px] font-semibold">All rights reserved</p>
+  <p class="text-[8px] font-semibold" class:start >All rights reserved</p>
 </div>
+
+<style>
+  .start {
+    @apply justify-start text-start;
+  }
+</style>
