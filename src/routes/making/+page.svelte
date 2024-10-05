0c948156c570f6afd7ea2568d97e77d094dadddc
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-10-05 14:16:32 -0300

    refactor: making - implement observer component

--- src/routes/making/+page.svelte
+++ src/routes/making/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import Hero from "./hero.svelte";
   import { onMount } from "svelte";
-  import ChapterOne from "./chapter_one.svelte";
   import { afterNavigate } from "$app/navigation";
   import { showColumnOneGrid } from "@/lib/helpers/grid";
+  import { GridRoutesTimeline } from "@/lib/animations/timelines/GridRoutesTimeline";
+
+  import ChapterOne from "./chapter_one.svelte";
+  import Observer from "@/components/observer/observer.svelte";
   import BuildingPerfection from "./building_perfection.svelte";
   import PageWrapper from "@/components/wrappers/page_wrapper.svelte";
-  import { GridRoutesTimeline } from "@/lib/animations/timelines/GridRoutesTimeline";
   import NavigateTransition from "@/components/transitions/navigate_transition.svelte";
 
   afterNavigate(() => {
@@ -26,4 +28,5 @@
     <ChapterOne />
     <BuildingPerfection />
   </PageWrapper>
+  <Observer progressTarget={0.92} progressMultiplier={14} />
 </NavigateTransition>
