6125099264e0075f389badfe08d747ad3b00b3ca
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-10-05 14:14:00 -0300

    refactor: improves menu column one

--- src/components/menu/menu_column_one.svelte
+++ src/components/menu/menu_column_one.svelte
@@ -1,24 +1,21 @@
 <div id="menu_column_one" class="h-svh relative bg-black -translate-x-full">
-  <div class="w-full h-full absolute top-0 bottom-0 left-0 right-0">
-    <div>
-      <img
-        class="absolute w-full h-full top-0 left-0 right-0 bottom-0 object-cover"
-        src="/images_grid/grid_one.webp"
-        alt=""
-      />
-      <div
-        class="absolute top-1/2 grid justify-items-center gap-4 -translate-y-1/2"
-      >
-        <img
-          id="logo_ferrari"
-          class="w-1/2"
-          src="/logo/logo.svg"
-          alt="Ferrari"
-        />
-        <p class="uppercase font-futura_bt text-2xl text-hells_red">
-          In theaters christmas
-        </p>
-      </div>
+  <div
+    id="column_one_wrapper"
+    class="w-full h-full absolute top-0 bottom-0 left-0 right-0 block z-[100] bg-hells_red"
+  ></div>
+  <div>
+    <img
+      class="absolute w-full h-full top-0 left-0 right-0 bottom-0 object-cover"
+      src="/images_grid/grid_one.webp"
+      alt=""
+    />
+    <div
+      class="absolute top-1/2 grid justify-items-center gap-4 -translate-y-1/2"
+    >
+      <img id="logo_ferrari" class="w-1/2" src="/logo/logo.svg" alt="Ferrari" />
+      <p class="uppercase font-futura_bt text-2xl text-hells_red">
+        In theaters christmas
+      </p>
     </div>
   </div>
 </div>
